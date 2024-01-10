0d853e96455e9a8dae434487796ae3f3c3d3a03c
Tech5818 <pqow5818@gmail.com>
2024-01-10 09:17:20 +0900

    :sparkles: fontawesome 설치

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { Mobile } from '../../Responsive';
+import { D_Header } from '../../style/header/header.style';
+
+export const Header = () => {
+  return (
+    <>
+      <Mobile>
+        <D_Header></D_Header>
+      </Mobile>
+    </>
+  );
+};
--- src/style/header/header.style.ts
+++ src/style/header/header.style.ts
@@ -1,5 +1,9 @@
 import { Flex, chakra } from '@chakra-ui/react';
 
 export const D_Header = chakra(Flex, {
-  baseStyle: {},
+  baseStyle: {
+    height: '60px',
+    background: '#fff',
+    boxShadow: '0 4px 20px -11px rgba(0, 0, 0, 0.25)',
+  },
 });
